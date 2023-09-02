b21081525df4c96e1e539942769b0d8fc62103f2
ananyasharma2010 <108532846+ananyasharma2010@users.noreply.github.com>
2023-09-02 09:43:23 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# bottomTabNavigation
+# wily-v2-PRO-C68
+Solution code for PRO-C68
---
+++ components/BottomTabNavigator.js
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+import { NavigationContainer } from "@react-navigation/native";
+//import createBottomTabNavigator libray -Challenge 1
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import TransactionScreen from "../screens/Transaction";
+import SearchScreen from "../screens/Search";
+
+//Add the createBottomTabNavigator to the variable called tab-Challenge 2
+export default class BottomTabNavigator extends Component {
+  render() {
+    return (
+      <NavigationContainer>
+        <Tab.Navigator>
+      //Add the code to move to the respective screen when tab is clicked-challenge 3
+         <Tab.Screen name="Transaction" component={TransactionScreen} />
+         <Tab.Screen name="Search" component={SearchScreen} />
+        </Tab.Navigator>
+      </NavigationContainer>
+    );
+  }
+}
